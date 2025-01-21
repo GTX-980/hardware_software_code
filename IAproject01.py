233328701c18b3f683069350ced32996c2eee59c
GTX-980 <abinacerisaias34@gmail.com>
2025-01-20 23:53:53 -0500

    poject

--- IAproject01.py
+++ IAproject01.py
@@ -1,22 +1,3 @@
-def question():
-    print("Do you like programming {}? (yes or no)-->".format(name))
-    answer = input().lower().strip()
-    if answer == "yes":
-        print("Great! You said {}!".format(answer))
-        print("We can't wait to teach you sone Python!-->")
-    elif answer == "no":
-        print("Hmm... You said {}?".format(answer))
-        print("Well we hope you change your mind once we start learning.-->")
-    else:
-        print("I do not understand what you mean by {}".format(answer))
-        question()
-    answer = input()
-    print("Before you go we have a few more questions {} -->".format(name))
-    answer = input()
-    print("What college are you going to {}? -->".format(name))
-    answer = input()
-    print("Now what highschool did you go to before coming to {}? -->".format(answer))
-    answer = input()
 def main():
     print("Hello user-->")
     answer = input()
@@ -26,7 +7,50 @@ def main():
     print()
     print("But first, we would like to know your name.-->")
     name = input()
-    question()
+    print("Do you like programming {}? (yes or no)-->".format(name))
+    answer0 = input().lower().strip()
+    if answer0 == "yes":
+        print("Great! You said {}!".format(answer0))
+        print("We can't wait to teach you sone Python!-->")
+    elif answer0 == "no":
+        print("Hmm... You said {}?".format(answer0))
+        print("Well we hope you change your mind once we start learning.-->")
+    else:
+        print("I do not understand what you mean by {}".format(answer0))
+        def question():
+            print("Do you like programming {}? (yes or no)-->".format(name))
+            answer0 = input().lower().strip()
+            if answer0 == "yes":
+                print("Great! You said {}!".format(answer0))
+                print("We can't wait to teach you sone Python!-->")
+            elif answer0 == "no":
+                print("Hmm... You said {}?".format(answer0))
+                print("Well we hope you change your mind once we start learning.-->")
+            else:
+                print("I do not understand what you mean by {}".format(answer0))
+                question()
+        question()
+    answer = input()
+    print("Before you go we have a few more questions {} -->".format(name))
+    answer = input()
+    print("What college are you going to {}? -->".format(name))
+    answer1 = input()
+    print("Now what highschool did you go to before coming to {}? -->".format(answer1))
+    answer2 = input()
+    print("which of the two did you like most? -->")
+    answer3 = input()
+    print("Amazing!")
+    print()
+    print("now i know that your name is {}".format(name))
+    print("you go to {}".format(answer1))
+    print("your highschool used to be {}". format(answer2))
+    print("you liked {} the most".format(answer3))
+    if answer0 == "yes":
+        print("and that you like programing!-->")
+    elif answer0 == "no":
+        print("and that your not too fond of programing.-->")
+    answer = input()
+    print("well thats all the time we have now see you later!")
 
 if __name__ == "__main__":
     main()
