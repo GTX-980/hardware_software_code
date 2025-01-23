3c2ce99f6272b06b7b4d6d49bd0bc4483af2df24
GTX-980 <abinacerisaias34@gmail.com>
2025-01-23 13:46:55 -0500

    classwork

---
+++ IAproject02.py
@@ -0,0 +1,8 @@
+def question():
+    print("Q1: What is your name?")
+    name = input()
+    print("Q2: What is your favorite car {}?".format(name))
+    answer = input()
+    print("Q3: what genre of music do you listen to the most {}".format(name))
+    answer = input()
+    print("Q4: ")
