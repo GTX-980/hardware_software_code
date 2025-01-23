727527b451ce8ac1c6e26aaef4e444e83ec7787a
GTX-980 <abinacerisaias34@gmail.com>
2025-01-23 14:45:21 -0500

    update

--- IAproject02.py
+++ IAproject02.py
@@ -1,8 +1,31 @@
 def question():
     print("Q1: What is your name?")
     name = input()
+    if name == "exit":
+        exit()
     print("Q2: What is your favorite car {}?".format(name))
     answer = input()
+    if answer == "exit":
+        exit()
     print("Q3: what genre of music do you listen to the most {}".format(name))
     answer = input()
+    if answer == "exit":
+        exit()
     print("Q4: ")
+    answer = input()
+    if answer == "exit":
+        exit()
+    question()
+def main():
+    print("Welcome user!")
+    print()
+    print("I will ask you 4 questions and repeat them over and over until")
+    print("you type the word Exit")
+    print()
+    print("I will also keep track of how many times you answered a queston")
+    print("as well as keep track of how many times you went through a full loop")
+    question()
+def exit():
+    quit()
+if __name__ == "__main__":
+    main()
